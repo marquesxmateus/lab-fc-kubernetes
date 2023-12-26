32b3799db0508f014a389097d21f3b3c45c2a763
Mateus Marques <marquesxmateus@gmail.com>
2023-12-26 15:56:21 -0300

    config service port vs targetport

--- server.go
+++ server.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// Create a new server
 	http.HandleFunc("/", Hello)
-	http.ListenAndServe(":8081", nil)
+	http.ListenAndServe(":8000", nil)
 	fmt.Println("Server is running on port 80")
 }
 
